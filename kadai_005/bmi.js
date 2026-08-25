124b65904a8f7f3705280aec66f7610c6f594fc8
nonoshitasachi <sachiyexia123@gmail.com>
2026-08-25 22:04:07 +0900

    再アップロード

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,7 @@
 //変数の宣言
 let userWeight = 68;
 let userHeight = 1.7;
+let userBmi = userWeight / (userHeight * userHeight);
 
 //BMIの出力
-console.log(userWeight / (userHeight * userHeight));
+console.log(userBmi);
