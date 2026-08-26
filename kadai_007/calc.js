45f1ea758a8d4fe99375057d9045d2b5a176945e
nonoshitasachi <sachiyexia123@gmail.com>
2026-08-27 07:29:07 +0900

    kadai_007あっぴ

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+let num = 16;
+
+switch (true) {
+  case num % 3 == 0 && num % 5 == 0:
+    console.log("3と5の倍数です");
+    break;
+  case num % 3 == 0:
+    console.log("3の倍数です");
+    break;
+  case num % 5 == 0:
+    console.log("5の倍数です");
+    break;
+  default:
+    console.log(num);
+    break;
+}
